d02b1de0f341d127f9d4667246e520e1066c4014
jacopo.ferrigno@gmail.com <jacopo.ferrigno@gmail.com@65eb150e-71e0-8ec6-5dee-a44731481414>
2012-09-06 18:38:51 +0000

    Release 0.1. Finally it works. Now lest go with optimizing.

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -1,11 +1,19 @@
 package gui;
 
 import java.awt.*;
-import java.security.KeyStore.Builder;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
+import javax.swing.*;
 
 import logic.*;
 
-import javax.swing.*;
+
+
+
+import solver.*;
 
 public class Gui extends JFrame {
 	private static final long serialVersionUID = 1L;
@@ -14,17 +22,32 @@ public class Gui extends JFrame {
 	
 	private JPanel buttonsPanel;
 	private JPanel menuPanel;
+	private JButton menus[];
 	private JPanel outputPanel;
+	private JPanel movesPanel;
 	private JButton board[][];
 	private JLabel solutionLabel;
+	private JTextField movesText;
+	private int moves;
+	
+	
 	//Images
 	private ImageIcon bubbles[];
+	//listener
+	private ActionListener boardLitener;
+	private ActionListener menuListener;
+	private ActionListener textListener;
 	
 	public Gui(){
+		
+		this.boardLitener=new Gui.boardListener();
+		this.menuListener=new Gui.menuListener();
+		this.textListener=new Gui.movesListener();
 		//Build main Menu
 		this.setTitle("Bubble Blast Solver");
-		this.setPreferredSize(new Dimension(this.xSize,this.ySize));
+		this.setPreferredSize(new Dimension(Gui.xSize,Gui.ySize));
 		this.setLocation(200, 200);
+		this.moves=-1;
 		//Border Layout used to divide center and East
 		this.setLayout(new BorderLayout());
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
@@ -32,13 +55,11 @@ public class Gui extends JFrame {
 		
 		
 		this.load_image();
-		this.buldMenu(this);
+		this.buildMenu(this);
 		this.buildBoard(this,null);
 		this.buildOutputLabel(this);
+		this.buildMovesText(this);
 	
-		BoardInterface tempTry=new Board();
-		tempTry.setBubble(2, 2, 3);
-		this.buildBoard(this, tempTry);
 		
 		
 		
@@ -51,6 +72,7 @@ public class Gui extends JFrame {
 		
 	}
 	
+
 	
 	private ImageIcon [] load_image(){
 		
@@ -67,13 +89,18 @@ public class Gui extends JFrame {
 		return this.bubbles;
 	}
 	
-	private void buldMenu(JFrame mainWindow){
+	private void buildMenu(JFrame mainWindow){
 		
+		this.menus=new JButton[2];
 		this.menuPanel=new JPanel();
 		this.menuPanel.setLayout(new BoxLayout(this.menuPanel, BoxLayout.Y_AXIS));
 		this.menuPanel.add(new Box.Filler(null, null, null));
-		this.menuPanel.add(new JButton("Solve!"));
-		this.menuPanel.add(new JButton("Clear  "));
+		this.menus[0]=new JButton("Solve!");
+		this.menus[0].addActionListener(this.menuListener);			
+		this.menuPanel.add(this.menus[0]);
+		this.menus[1]=new JButton("Clear  ");
+		this.menus[1].addActionListener(this.menuListener);
+		this.menuPanel.add(this.menus[1]);
 		this.menuPanel.add(new Box.Filler(null, null, null));
 		
 		mainWindow.add(this.menuPanel,BorderLayout.EAST);
@@ -94,7 +121,7 @@ public class Gui extends JFrame {
 				 * 
 				 */
 					this.board[i][j]=new JButton(this.bubbles[bubbleBoard == null? 0 : bubbleBoard.getBubbleValue(i, j) ]);
-				
+					this.board[i][j].addActionListener(this.boardLitener);
 			
 					this.buttonsPanel.add(this.board[i][j]);
 				}
@@ -103,8 +130,11 @@ public class Gui extends JFrame {
 			
 		
 		mainWindow.add(this.buttonsPanel,BorderLayout.CENTER);
+		
 	}
 
+
+	
 	private void buildOutputLabel(JFrame mainWindow){
 		this.outputPanel=new JPanel(new BorderLayout());
 		this.solutionLabel=new JLabel("Soluzione");
@@ -112,5 +142,173 @@ public class Gui extends JFrame {
 		mainWindow.add(this.outputPanel, BorderLayout.SOUTH);
 		
 	}
+	
+	private void buildMovesText(JFrame mainWindow){
+		this.movesPanel=new JPanel(new BorderLayout());
+		this.movesText=new JTextField(1);
+		this.movesText.addActionListener(Gui.this.textListener);
+		this.movesPanel.add(this.movesText,BorderLayout.CENTER);
+		this.movesPanel.add(new JLabel("Moves:"),BorderLayout.WEST);
+		this.movesPanel.add(new Box.Filler(null,null,null ),BorderLayout.EAST);
+		mainWindow.add(this.movesPanel, BorderLayout.NORTH);
+		
+		
+		
+	}
+	
+	public Gui clear(){
+		
+		for (int i=0; i< BoardInterface.yMaxSize; i++){
+			for (int j=0; j< BoardInterface.xMaxSize; j++){
+			
+				this.board[i][j].setIcon(this.bubbles[0]);
+			}
+			this.movesText.setText("");
+			this.solutionLabel.setText("Soluzione");
+		}
+		
+		return this;
+		
+		
+	}
+	
+//Action listener
+	private class boardListener implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			JButton button=((JButton) e.getSource());
+			int value=Integer.parseInt(button.getIcon().toString());
+			button.setIcon(Gui.this.bubbles[(value+1)%5]);
+		 
+		}
+		
+		
+		
+		
+		
+		
+	}
+	
+	private class menuListener implements ActionListener{
+
+		
+		BoardInterface bboard;
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			JButton button=((JButton) e.getSource());
+			
+			if(button.getText().equals("Solve!")){
+				
+				Gui.this.menus[0].setEnabled(false);
+				Gui.this.menus[1].setEnabled(false);
+				Gui.this.solutionLabel.setText(button.getText());
+				try{
+					Gui.this.moves=Integer.parseInt(Gui.this.movesText.getText());
+					if(Gui.this.moves<0){
+						Gui.this.solutionLabel.setText( Gui.this.moves +" is not a valid value: moves must be >=0");
+						Gui.this.movesText.setText("");
+					
+					}else{
+						
+						this.buildBoard();
+						this.solve();
+						
+						
+					}
+					
+					return;
+				}
+				catch(NumberFormatException ne){
+					Gui.this.solutionLabel.setText(Gui.this.movesText.getText() +" is not a valid value");
+					Gui.this.movesText.setText("");
+				}
+				
+				finally{
+					Gui.this.menus[0].setEnabled(true);
+					Gui.this.menus[1].setEnabled(true);
+					
+				}
+				
+			}
+			if(button.getText().equals("Clear  ")){
+				Gui.this.clear();
+				
+			}	
+			
+		}
+		
+		private menuListener buildBoard(){
+			this.bboard=new Board();
+			for (int i=0; i< BoardInterface.yMaxSize; i++){
+                for(int j=0; j< BoardInterface.xMaxSize; j++ ){
+                	int bvalue=Integer.parseInt(Gui.this.board[i][j].getIcon().toString());
+                	if(bvalue==0)bvalue=5;
+                	this.bboard.setBubble(i, j, bvalue);
+                	
+                	
+                }
+			}
+			
+			
+			return this;
+		}
+		
+		private menuListener solve(){
+			BoardSolverInterface boardSolver=new BoardSolver();
+            boardSolver.init(this.bboard);
+            boardSolver.setMoves(Gui.this.moves);
+            boardSolver.solve();
+            if(!boardSolver.isSolved()){
+            
+            	Gui.this.solutionLabel.setText("No solution Found. :-(");
+            	
+            }
+            else{
+            	
+            	Gui.this.solutionLabel.setText(boardSolver.toString());
+            	
+            }
+			
+			
+			
+			return this;
+		}
+		
+		
+		
+	}
+
+	private class movesListener implements ActionListener{
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+				JTextField text=(JTextField)e.getSource();
+				
+				try{
+					Gui.this.moves=Integer.parseInt(text.getText());
+					if(Gui.this.moves<0){
+						Gui.this.solutionLabel.setText( Gui.this.moves +" is not a valid value: moves must be >=0");
+						
+					}
+					Gui.this.solutionLabel.setText("Soluzione");
+				
+				}
+				catch(NumberFormatException ne){
+					Gui.this.solutionLabel.setText(text.getText() +"is not a valid value");
+					
+				}
+				
+			
+		}
+
+		
+	
+		
+	}
+
 
 }
